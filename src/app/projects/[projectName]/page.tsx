1d5ec4f1a641ed4ee1d1d84e08d3243e0fe11dbe
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2025-08-20 17:32:39 -0400

    routing works now

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -1,16 +1,14 @@
 // app/projects/[projectName]/page.tsx
 import ProjectPage from "@/components/ProjectPage";
-import { learnOrgoChem, financeTracker } from "../../../data/project";
+import { learnOrgoChem, financeTracker } from "@/data/project";
 
 interface Props {
   params: Promise<{ projectName: string }>;
 }
 
-// Use a default exported function
 export default async function Project({ params }: Props) {
   const { projectName } = await params;
 
-  // Map project [project] to objects
   const projectsMap: Record<string, any> = {
     "finance-app": financeTracker,
     "learn-orgo-chem": learnOrgoChem,
